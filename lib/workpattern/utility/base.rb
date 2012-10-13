9ef8e7f4baebf1c14a3dc5b468b01160e18e0a85
Barrie Callender <barrie@callenb.org>
2012-10-13 22:54:26 +1000

    YARD documentation for base.rb

--- lib/workpattern/utility/base.rb
+++ lib/workpattern/utility/base.rb
@@ -1,12 +1,29 @@
 module Workpattern
+
+  # Mixins expected to be used in more than one class
+  #
   # @since 0.2.0
   #
   module Utility
-  
+
+    # Returns the supplied <tt>DateTime</tt> at the very start of the day.
+    #
+    # @param [DateTime] adate is the <tt>DateTime</tt> to be changed
+    # @return [DateTime] 
+    # 
+    # @todo Consider mixin for DateTime class 
+    #
     def midnight_before(adate)
       return adate -(HOUR * adate.hour) - (MINUTE * adate.min)
     end
     
+    # Returns the supplied <tt>DateTime</tt> at the very start of the next day.
+    #
+    # @param [DateTime] adate is the <tt>DateTime</tt> to be changed
+    # @return [DateTime] 
+    # 
+    # @todo Consider mixin for DateTime class  
+    #
     def midnight_after(adate)
       return midnight_before(adate.next_day)
     end
