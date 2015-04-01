2591a7adfc16644cc0db18eaa4b23ccf96b8147f
Barrie Callender <barrie@callenb.org>
2015-04-01 20:51:21 +0100

    #test_must_be_161_minutes_after_21_19

--- lib/workpattern/day.rb
+++ lib/workpattern/day.rb
@@ -2,21 +2,43 @@ module Workpattern
 
   class Day
 
-    attr_accessor :hours_per_day, :total
+    attr_accessor :value, :hours_per_day, :total
 
     def initialize(type=1,hours_per_day=24)
       @hours_per_day = hours_per_day
       @value = working_day * type
     end
-
+    
     def total
-     @value.to_s(2).count('1')
+     working_minutes_in @value
     end
+
+    def minutes_remaining(time)
+      working_minutes_in remaining_binary_minutes(time)
+    end
+
 
     private
 
     def working_day
       2**(60*self.hours_per_day)-1
+    end
+
+    def working_minutes_in value
+      value.to_s(2).count('1')
+    end
+
+    def binary_time(hour,minute)
+      2**( (hour * 60) + minute )
+    end
+
+    def work_to_end_of_day(time)
+      binary_time(self.hours_per_day,0) - binary_time(time.hour, time.min)
+    end
+
+    def remaining_binary_minutes(time)
+      mask = work_to_end_of_day(time)
+      (self.value & mask)
     end
 
   end
--- test/test_day.rb
+++ test/test_day.rb
@@ -2,9 +2,30 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestDay < MiniTest::Unit::TestCase #:nodoc:
 
+  def setup
+    @working = Workpattern::Day.new(1)
+  end
+
   def test_must_create_a_default_day_as_working
     working_day = Workpattern::Day.new()
     assert_equal (24*60), working_day.total
   end
 
+  def test_must_create_a_working_day
+    working_day = Workpattern::Day.new(1)
+    assert_equal (24*60), working_day.total
+  end
+
+  def test_must_create_a_resting_day
+    resting_day = Workpattern::Day.new(0)
+    assert_equal 0, resting_day.total
+  end
+
+  def test_must_be_161_minutes_after_21_19
+    assert_equal 161, @working.minutes_remaining(clock(21,19))
+  end 
+
+  def clock(hours,minutes)
+    DateTime.new(2000,1,1,hours,minutes)
+  end
 end
