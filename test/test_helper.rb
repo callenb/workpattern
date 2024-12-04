69ac15cf1c7d42da622eaa6ac54790f4ec09925e
Barrie Callender <barrie@callenb.org>
2024-12-04 00:25:28 +0000

    removed code dealing with ruby versions before v3.x.x

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,5 @@
 require 'minitest/autorun'
 require File.dirname(__FILE__) + '/../lib/workpattern.rb'
 
-if RUBY_VERSION < "2"
-    class WorkpatternTest < MiniTest::Unit::TestCase
-    end
-else
-    class WorkpatternTest < MiniTest::Test
-    end
+class WorkpatternTest < Minitest::Test
 end
