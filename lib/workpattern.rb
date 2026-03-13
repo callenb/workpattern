35c3f3d11d0ca9681509c78fae89509469fe542a
Barrie Callender <barrie@usingprimavera.com>
2026-03-13 18:16:27 +0000

    upd: Modernisation Phase 2: require sorted_set unconditionally

--- lib/workpattern.rb
+++ lib/workpattern.rb
@@ -9,7 +9,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__)) unless
   $LOAD_PATH.include?(File.dirname(__FILE__)) || $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
 
 
-require 'sorted_set' if RUBY_VERSION >= "2.4"
+require 'sorted_set'
 require 'date'
 require 'workpattern/clock'
 require 'workpattern/constants'
