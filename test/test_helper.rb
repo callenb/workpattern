50aed7188bbe678f668fb4ad99f0fa0cf6e62f48
Barrie Callender <barrie@usingprimavera.com>
2026-05-26 23:59:23 +0100

    upd: fixed rubocop offences on `test/test_helper.rb`
    See `.rubocop_todo.yml` for list of offences

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require File.dirname(__FILE__) + '/../lib/workpattern.rb'
+require "#{File.dirname(__FILE__)}/../lib/workpattern.rb"
 
 class WorkpatternTest < Minitest::Test
 end
