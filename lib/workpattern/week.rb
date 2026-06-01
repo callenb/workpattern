5624519a34c1ba9a9eff6085ef6db41c15b1725b
Barrie Callender <barrie@usingprimavera.com>
2026-06-01 08:24:36 +0100

    upd:  replace .inject with .sum

--- lib/workpattern/week.rb
+++ lib/workpattern/week.rb
@@ -144,6 +144,8 @@ module Workpattern
 
     def minutes_in_day_range(first, last)
       @days[first..last].inject(0) { |sum, day| sum + day.working_minutes }
+      @days[first..last].sum { |day| 1 * day.working_minutes }
+
     end
 
     def add(a_date, a_duration)
